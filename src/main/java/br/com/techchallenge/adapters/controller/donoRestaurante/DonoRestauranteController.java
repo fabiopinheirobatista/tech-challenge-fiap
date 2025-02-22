0bdbf8425c82932a6a459b1c7c8423278d189893
Fabio <42181125+fabiopinheirobatista@users.noreply.github.com>
2025-02-22 19:32:35 -0300

    Refactoring the Restaurant Owners and Restaurant packages

--- src/main/java/br/com/techchallenge/adapters/controller/donoRestaurante/DonoRestauranteController.java
+++ src/main/java/br/com/techchallenge/adapters/controller/donoRestaurante/DonoRestauranteController.java
@@ -1,14 +1,19 @@
 package br.com.techchallenge.adapters.controller.donoRestaurante;
 
-import br.com.techchallenge.infra.dto.donoRestaurante.request.DonoRestauranteRequestDto;
 import br.com.techchallenge.adapters.useCaseImpl.BuscarDonoRestaurantePorIdUseCase;
 import br.com.techchallenge.adapters.useCaseImpl.CadastrarDonoRestauranteUseCase;
+import br.com.techchallenge.application.mapper.DonoRestauranteMapper;
 import br.com.techchallenge.domain.DonoRestaurante;
+import br.com.techchallenge.infra.dto.donoRestaurante.request.DonoRestauranteRequestDto;
+import br.com.techchallenge.infra.entity.DonoRestauranteEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 
@@ -19,6 +24,7 @@ public class DonoRestauranteController {
 
     private final CadastrarDonoRestauranteUseCase cadastrarDonoRestauranteUseCase;
     private final BuscarDonoRestaurantePorIdUseCase buscarDonoRestaurantePorIdUseCase;
+    private final DonoRestauranteMapper mapper;
 
     @PostMapping
     public ResponseEntity<String> cadastrar(@RequestBody DonoRestauranteRequestDto request) {
@@ -32,7 +38,10 @@ public class DonoRestauranteController {
                     LocalDate.now()
             );
 
+            DonoRestauranteEntity entity = mapper.toDonoRestauranteEntity(dono);
+
             DonoRestaurante donoSalvo = cadastrarDonoRestauranteUseCase.cadastrar(dono);
+
             return new ResponseEntity<>("Dono de Restaurante cadastrado com sucesso", HttpStatus.CREATED);
         } catch (DataIntegrityViolationException e) {
             return new ResponseEntity<>("Dono de Restaurante já cadastrado com essas informações", HttpStatus.CONFLICT);
