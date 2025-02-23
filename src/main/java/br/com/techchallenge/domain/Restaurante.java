d7bcd1f76246e3e01a3a7c700a2d109e24147a52
Fabio <42181125+fabiopinheirobatista@users.noreply.github.com>
2025-02-22 22:34:56 -0300

    Refactoring the Restaurant Owners and Restaurant packages

--- src/main/java/br/com/techchallenge/domain/Restaurante.java
+++ src/main/java/br/com/techchallenge/domain/Restaurante.java
@@ -1,17 +1,11 @@
 package br.com.techchallenge.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
-@Table(name = "restaurante")
+@Table(name = "restaurantes")
 @NoArgsConstructor
 @AllArgsConstructor
 public class Restaurante {
@@ -19,8 +13,14 @@ public class Restaurante {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String nome;
+
+    @Column(nullable = false)
     private String endereco;
+
+    @Column(nullable = false)
     private String tipoCozinha;
 
     public Restaurante(String nome, String endereco, String tipoCozinha) {
