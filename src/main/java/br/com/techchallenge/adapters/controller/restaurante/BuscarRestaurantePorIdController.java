9dbf0eb2011f324c54da816767f9fcd433c6cc3d
ramaro <ricardoamaro@lithia.com>
2025-02-25 21:55:31 -0300

    adicionando dados do dono do restaurante ao response de busca de restaurante por id

--- src/main/java/br/com/techchallenge/adapters/controller/restaurante/BuscarRestaurantePorIdController.java
+++ src/main/java/br/com/techchallenge/adapters/controller/restaurante/BuscarRestaurantePorIdController.java
@@ -2,6 +2,8 @@ package br.com.techchallenge.adapters.controller.restaurante;
 
 import br.com.techchallenge.adapters.UseCaseImpl.restaurante.BuscarRestaurantePorIdUseCase;
 import br.com.techchallenge.domain.Restaurante;
+import br.com.techchallenge.infra.dto.donoRestaurante.response.DonoRestauranteSimplesResponseDto;
+import br.com.techchallenge.infra.dto.restaurante.response.RestauranteResponseDto;
 import br.com.techchallenge.shared.InternalServerErrorException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -25,7 +27,20 @@ public class BuscarRestaurantePorIdController {
         try {
             Optional<Restaurante> restaurante = buscarRestaurantePorIdUseCase.buscarPorId(id);
             if (restaurante.isPresent()) {
-                return ResponseEntity.ok(restaurante.get());
+
+                RestauranteResponseDto responseDto = new RestauranteResponseDto(
+                        restaurante.get().getId(),
+                        restaurante.get().getNome(),
+                        restaurante.get().getEndereco(),
+                        restaurante.get().getTipoCozinha(),
+                        restaurante.get().getDonoRestaurante() != null ? new DonoRestauranteSimplesResponseDto(
+                                restaurante.get().getDonoRestaurante().getNome(),
+                                restaurante.get().getDonoRestaurante().getEmail(),
+                                restaurante.get().getDonoRestaurante().getEndereco()
+                        ) : null
+                );
+
+                return ResponseEntity.ok(responseDto);
             } else {
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Restaurante não encontrado");
             }
