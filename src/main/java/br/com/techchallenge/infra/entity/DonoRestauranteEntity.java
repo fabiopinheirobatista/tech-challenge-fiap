9edeb617882812d51d3973050d554c34f77aaf58
Fabio <42181125+fabiopinheirobatista@users.noreply.github.com>
2025-03-02 19:35:34 -0300

    Aprovechamiento de Lombok en entidades

--- src/main/java/br/com/techchallenge/infra/entity/DonoRestauranteEntity.java
+++ src/main/java/br/com/techchallenge/infra/entity/DonoRestauranteEntity.java
@@ -2,14 +2,14 @@ package br.com.techchallenge.infra.entity;
 
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "donos_restaurante")
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString(exclude = {"senha"})
@@ -36,58 +36,6 @@ public class DonoRestauranteEntity {
 
     @Column(name = "data_ultima_alteracao")
     private LocalDate dataUltimaAlteracao;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public LocalDate getDataUltimaAlteracao() {
-        return dataUltimaAlteracao;
-    }
 
     public void setDataUltimaAlteracao(LocalDate dataUltimaAlteracao) {
         this.dataUltimaAlteracao = dataUltimaAlteracao;
