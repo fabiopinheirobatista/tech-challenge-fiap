6b4e23ce7d04f95bc1b4f2a642851af53c142fd3
Fabio <42181125+fabiopinheirobatista@users.noreply.github.com>
2025-03-05 11:45:01 -0300

    Adding resources

--- src/main/java/br/com/techchallenge/adapters/dto/Restaurante/RestauranteResponseDTO.java
+++ src/main/java/br/com/techchallenge/adapters/dto/Restaurante/RestauranteResponseDTO.java
@@ -1,11 +1,12 @@
 package br.com.techchallenge.adapters.dto.Restaurante;
 
 import br.com.techchallenge.domain.endereco.Endereco;
+import br.com.techchallenge.adapters.dto.donoRestaurante.DonoRestauranteResponseDTO;
 
 public record RestauranteResponseDTO(
         Long id,
         String nome,
         Endereco endereco,
         String tipoCozinha,
-        br.com.techchallenge.adapters.dto.donoRestaurante.DonoRestauranteResponseDTO donoRestaurante
+        DonoRestauranteResponseDTO donoRestaurante
 ) {}
