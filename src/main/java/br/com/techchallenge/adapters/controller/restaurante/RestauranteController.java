2971492801a9c499d1aa848b28b6b6de4cec9b02
Fabio <42181125+fabiopinheirobatista@users.noreply.github.com>
2025-03-06 09:11:54 -0300

    Resolving conflict with the alter method

--- src/main/java/br/com/techchallenge/adapters/controller/restaurante/RestauranteController.java
+++ src/main/java/br/com/techchallenge/adapters/controller/restaurante/RestauranteController.java
@@ -56,24 +56,24 @@ public class RestauranteController {
         }
     }
 
-//    @PutMapping("/atualizar/{id}")
-//    public ResponseEntity<?> atualizar(@PathVariable Long id, @RequestBody RestauranteRequestDTO request) {
-//        try {
-//            RestauranteEntity restaurante = service.buscarPorId(id)
-//                    .orElseThrow(() -> new EntityNotFoundException("Restaurante não encontrado"));
-//
-//            restaurante.setNome(request.nome());
-//            restaurante.setEndereco(request.endereco());
-//            restaurante.setTipoCozinha(request.tipoCozinha());
-//
-//            service.salvar((restaurante), request.donoRestaurante());
-//            return ResponseEntity.ok("Restaurante atualizado com sucesso");
-//        } catch (EntityNotFoundException e) {
-//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Restaurante não encontrado");
-//        } catch (Exception e) {
-//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao atualizar restaurante");
-//        }
-//    }
+    @PutMapping("/atualizar/{id}")
+    public ResponseEntity<?> atualizar(@PathVariable Long id, @RequestBody RestauranteRequestDTO request) {
+        try {
+            RestauranteEntity restaurante = service.buscarPorId(id)
+                    .orElseThrow(() -> new EntityNotFoundException("Restaurante não encontrado"));
+
+            restaurante.setNome(request.getNome());
+            restaurante.setEndereco(request.endereco());
+            restaurante.setTipoCozinha(request.tipoCozinha());
+
+            service.salvar(restaurante);
+            return ResponseEntity.ok("Restaurante atualizado com sucesso");
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Restaurante não encontrado");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao atualizar restaurante");
+        }
+    }
 
     @GetMapping("/listar/{id}")
     public ResponseEntity<?> buscarPorId(@PathVariable Long id) {
