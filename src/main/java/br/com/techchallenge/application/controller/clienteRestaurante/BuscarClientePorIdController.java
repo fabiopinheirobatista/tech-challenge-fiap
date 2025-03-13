3452112dc00acf9329cc1c7273fca93e39f750c6
Fabio <42181125+fabiopinheirobatista@users.noreply.github.com>
2025-03-13 17:09:49 -0300

    Ajustando retorno dos Clientes

--- src/main/java/br/com/techchallenge/application/controller/clienteRestaurante/BuscarClientePorIdController.java
+++ src/main/java/br/com/techchallenge/application/controller/clienteRestaurante/BuscarClientePorIdController.java
@@ -3,8 +3,8 @@ package br.com.techchallenge.application.controller.clienteRestaurante;
 
 import br.com.techchallenge.domain.output.clienteRestaurante.ClienteRestauranteResponseDto;
 import br.com.techchallenge.domain.useCase.clienteRestaurante.BuscarClientePorIdUseCase;
-import br.com.techchallenge.infra.repository.ClienteRestauranteRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,13 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class BuscarClientePorIdController {
 
-    private final ClienteRestauranteRepository clienteRestauranteRepository;
     private final BuscarClientePorIdUseCase buscarClientePorIdUseCase;
 
     @GetMapping("/listar/{id}")
     public ResponseEntity<?> buscarPorId(@PathVariable Long id) {
-
-        ClienteRestauranteResponseDto cliente = buscarClientePorIdUseCase.execute(id);
-        return ResponseEntity.ok(cliente);
+        try {
+            ClienteRestauranteResponseDto cliente = buscarClientePorIdUseCase.execute(id);
+            return ResponseEntity.ok(cliente);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cliente não encontrado");
+        }
     }
+
 }
