0d7cce94cb12f009311818960dad5b02df31566f
Fabio <42181125+fabiopinheirobatista@users.noreply.github.com>
2025-03-15 23:06:43 -0300

    Changing Controller to exclude Service classes and applying the use of UseCase

--- src/main/java/br/com/techchallenge/application/controller/donoRestaurante/DonoRestauranteAlterarSenhaController.java
+++ src/main/java/br/com/techchallenge/application/controller/donoRestaurante/DonoRestauranteAlterarSenhaController.java
@@ -1,8 +1,9 @@
 package br.com.techchallenge.application.controller.donoRestaurante;
 
 import br.com.techchallenge.domain.input.donoRestaurante.DonoRestauranteAlterarSenhaRequestDTO;
+import br.com.techchallenge.domain.useCase.donoRestaurante.AlterarSenhaDonoRestauranteUseCase;
 import br.com.techchallenge.infra.converter.donoRestaurante.DonoRestauranteDTOConverter;
-import br.com.techchallenge.infra.service.DonoRestauranteService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -12,21 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/donos-restaurante")
+@RequiredArgsConstructor
 public class DonoRestauranteAlterarSenhaController {
 
     private final DonoRestauranteDTOConverter converter;
-    private final DonoRestauranteService service;
-
-    public DonoRestauranteAlterarSenhaController(DonoRestauranteDTOConverter converter, DonoRestauranteService service) {
-        this.converter = converter;
-        this.service = service;
-    }
+    private final AlterarSenhaDonoRestauranteUseCase alterarSenhaUseCase;
 
     @PutMapping("/alterar-senha")
     public ResponseEntity<String> alterarSenha(@RequestBody DonoRestauranteAlterarSenhaRequestDTO request) {
-        boolean atualizado = service.alterarSenha(request.getId(), request.getEmail(), request.getSenhaAtual(), request.getNovaSenha());
+        boolean atualizado = alterarSenhaUseCase.alterarSenha(
+                request.getId(),
+                request.getEmail(),
+                request.getSenhaAtual(),
+                request.getNovaSenha());
         if (!atualizado) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Atualização não realizada pois o ID informado não foi localizado ou o email/senha estão incorretos!");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Atualização não realizada pois o ID informado não foi localizado ou o email/senha estão incorretos!");
         }
         return ResponseEntity.status(HttpStatus.OK).body("Atualização realizada com sucesso!");
     }
