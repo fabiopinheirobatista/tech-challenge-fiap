81f47a6526a808ae872c6f7db4738112c2f87291
Fabio <42181125+fabiopinheirobatista@users.noreply.github.com>
2025-03-15 22:17:22 -0300

    Performing refactoring on the Restaurant Owners entity

--- src/main/java/br/com/techchallenge/domain/useCase/donoRestaurante/BuscarTodosDonoRestauranteUseCase.java
+++ src/main/java/br/com/techchallenge/domain/useCase/donoRestaurante/BuscarTodosDonoRestauranteUseCase.java
@@ -1,4 +1,41 @@
 package br.com.techchallenge.domain.useCase.donoRestaurante;
 
+import br.com.techchallenge.domain.gateway.donoRestaurante.BuscarTodosDonoRestauranteInterface;
+import br.com.techchallenge.domain.output.donoRestaurante.DonoRestauranteListarTodosResponseDTO;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Component
+@RequiredArgsConstructor
 public class BuscarTodosDonoRestauranteUseCase {
+
+    private final BuscarTodosDonoRestauranteInterface buscarDonoRestauranteInterface;
+
+    public List<DonoRestauranteListarTodosResponseDTO> execute() {
+        List<DonoRestauranteListarTodosResponseDTO> listaDonoRestauranteDTO = new ArrayList<>();
+        try {
+            List<DonoRestaurante> listaDonoRestaurante = buscarDonoRestauranteInterface.buscarTodos();
+
+            for (DonoRestaurante donoRestaurante : listaDonoRestaurante) {
+                listaDonoRestauranteDTO.add(new DonoRestauranteListarTodosResponseDTO(
+                        donoRestaurante.getId(),
+                        donoRestaurante.getNome(),
+                        donoRestaurante.getEndereco().toString(),
+                        donoRestaurante.getEmail(),
+                        donoRestaurante.getLogin()
+                ));
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao buscar Donos de Restaurante" + e.getMessage());
+        }
+
+        return listaDonoRestauranteDTO;
+    }
+
 }
--- src/main/java/br/com/techchallenge/domain/useCase/donoRestaurante/DeletarDonoRestauranteUseCase.java
+++ src/main/java/br/com/techchallenge/domain/useCase/donoRestaurante/DeletarDonoRestauranteUseCase.java
@@ -1,4 +1,24 @@
 package br.com.techchallenge.domain.useCase.donoRestaurante;
 
+import br.com.techchallenge.domain.exception.DonoRestauranteNaoEncontradoException;
+import br.com.techchallenge.domain.gateway.donoRestaurante.DeletarDonoRestauranteInterface;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+@RequiredArgsConstructor
 public class DeletarDonoRestauranteUseCase {
+
+    private final DeletarDonoRestauranteInterface donoRestauranteInterface;
+
+    public void execute(Long id) {
+        try {
+            donoRestauranteInterface.deletar(id);
+        } catch (Exception | DonoRestauranteNaoEncontradoException e) {
+            throw new RuntimeException("Erro ao deletar Dono de Restaurante" + e.getMessage());
+        }
+    }
+
 }
