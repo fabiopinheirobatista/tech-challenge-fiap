da2ba139016de16a7e36f6bd5e8393fa785f12b2
Fabio <42181125+fabiopinheirobatista@users.noreply.github.com>
2025-03-15 22:01:36 -0300

    Performing refactoring on the Restaurant Owners entity

--- src/main/java/br/com/techchallenge/domain/gateway/donoRestaurante/BuscarPorIdDonoRestauranteInterface.java
+++ src/main/java/br/com/techchallenge/domain/gateway/donoRestaurante/BuscarPorIdDonoRestauranteInterface.java
@@ -1,9 +1,10 @@
 package br.com.techchallenge.domain.gateway.donoRestaurante;
 
+import br.com.techchallenge.domain.exception.DonoRestauranteNaoEncontradoException;
 import br.com.techchallenge.domain.useCase.donoRestaurante.DonoRestaurante;
 
 public interface BuscarPorIdDonoRestauranteInterface {
 
-    DonoRestaurante buscarPorId(Long id);
+    DonoRestaurante buscarPorId(Long id) throws DonoRestauranteNaoEncontradoException;
 
 }
--- src/main/java/br/com/techchallenge/domain/useCase/donoRestaurante/BuscarPorIdDonoRestauranteUseCase.java
+++ src/main/java/br/com/techchallenge/domain/useCase/donoRestaurante/BuscarPorIdDonoRestauranteUseCase.java
@@ -1,5 +1,35 @@
 package br.com.techchallenge.domain.useCase.donoRestaurante;
 
+import br.com.techchallenge.domain.exception.DonoRestauranteNaoEncontradoException;
+import br.com.techchallenge.domain.gateway.donoRestaurante.BuscarPorIdDonoRestauranteInterface;
+import br.com.techchallenge.domain.output.donoRestaurante.DonoRestauranteListarIdResponseDTO;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+@RequiredArgsConstructor
 public class BuscarPorIdDonoRestauranteUseCase {
+
+    private final BuscarPorIdDonoRestauranteInterface donoRestauranteInterface;
+
+    public DonoRestauranteListarIdResponseDTO execute(Long id) {
+        DonoRestauranteListarIdResponseDTO donoRestauranteDTO = null;
+        try {
+            DonoRestaurante donoRestaurante = donoRestauranteInterface.buscarPorId(id);
+            donoRestauranteDTO = new DonoRestauranteListarIdResponseDTO(
+                    donoRestaurante.getId(),
+                    donoRestaurante.getNome(),
+                    donoRestaurante.getEndereco().toString(),
+                    donoRestaurante.getEmail(),
+                    donoRestaurante.getLogin()
+            );
+        } catch (Exception | DonoRestauranteNaoEncontradoException e) {
+            throw new RuntimeException("Erro ao buscar Dono de Restaurante pelo ID " + e.getMessage());
+        }
+
+        return donoRestauranteDTO;
+    }
 
 }
