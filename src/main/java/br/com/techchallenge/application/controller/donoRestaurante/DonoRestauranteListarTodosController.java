8495a9759e0bc8080aa60a2522b3e5a38c688946
Fabio <42181125+fabiopinheirobatista@users.noreply.github.com>
2025-03-15 23:29:39 -0300

    Changing Controller to exclude Service classes and applying the use of UseCase

--- src/main/java/br/com/techchallenge/application/controller/donoRestaurante/DonoRestauranteListarTodosController.java
+++ src/main/java/br/com/techchallenge/application/controller/donoRestaurante/DonoRestauranteListarTodosController.java
@@ -1,9 +1,7 @@
 package br.com.techchallenge.application.controller.donoRestaurante;
 
 import br.com.techchallenge.domain.output.donoRestaurante.DonoRestauranteListarTodosResponseDTO;
-import br.com.techchallenge.infra.converter.donoRestaurante.DonoRestauranteDTOConverter;
-import br.com.techchallenge.infra.entity.DonoRestauranteEntity;
-import br.com.techchallenge.infra.service.DonoRestauranteService;
+import br.com.techchallenge.domain.useCase.donoRestaurante.BuscarTodosDonoRestauranteUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,30 +9,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/donos-restaurante")
 @RequiredArgsConstructor
 public class DonoRestauranteListarTodosController {
 
-    private final DonoRestauranteDTOConverter converter;
-    private final DonoRestauranteService service;
+    private final BuscarTodosDonoRestauranteUseCase buscarTodosDonoRestauranteUseCase;
 
     @GetMapping("/listar-todos")
     public ResponseEntity<?> buscarTodos() {
         try {
-            List<DonoRestauranteEntity> donos = service.buscarTodos();
-            if (donos.isEmpty()) {
+            List<DonoRestauranteListarTodosResponseDTO> response = buscarTodosDonoRestauranteUseCase.execute();
+            if (response.isEmpty()) {
                 return ResponseEntity.status(HttpStatus.NOT_FOUND)
                         .body("Não existem Donos de Restaurante cadastrados.");
             }
-            List<DonoRestauranteListarTodosResponseDTO> response = donos.stream()
-                    .sorted(Comparator.comparing(DonoRestauranteEntity::getId))
-                    .map(converter::entityParaListarTodosDto)
-                    .collect(Collectors.toList());
             return ResponseEntity.ok(response);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
